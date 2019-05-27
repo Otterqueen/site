49dcc36b22e6e0a8e11dd53e037b20a443482fad
Unknown <mchapard@student.le-101.fr>
2019-05-27 19:13:05 +0200

    gall ok et fond noir header ok

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,29 +9,13 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 export class AboutMeComponent implements OnInit {
        
   imgs = [
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back1.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back2.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back3.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back4.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back5.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('../../assets/back6.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back1.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back2.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back3.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back4.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back5.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back6.jpg')`)},
   ]
-
-  // imgs = [
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back1.jpg'),
-  //    lol: 'miaou'},
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back2.jpg'),
-  //    lol: 'miaou'},
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back3.jpg'),
-  //    lol: 'miaou'},
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back4.jpg'),
-  //    lol: 'miaou'},
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back5.jpg'),
-  //    lol: 'miaou'},
-  //   {src: this.sanitization.bypassSecurityTrustUrl('../../assets/back6.jpg'),
-  //    lol: 'miaou'},
-  // ]
-
 
   constructor(private sanitization: DomSanitizer) { }
 
