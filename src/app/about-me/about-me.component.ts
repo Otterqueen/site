c95cb73d821d8e4c78b2ae7b8225039d43dc1511
Unknown <mchapard@student.le-101.fr>
2019-05-28 19:49:02 +0200

    gallery avec bonne photo ok

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,12 +9,24 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 export class AboutMeComponent implements OnInit {
        
   imgs = [
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back1.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back2.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back3.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back4.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back5.jpg')`)},
-    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/back6.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/1.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/2.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/3.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/4.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/5.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/6.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/7.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/8.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/9.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/10.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/11.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/12.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/13.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/14.png')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/16.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/17.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/18.jpg')`)},
+    {src: this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/19.jpg')`)},
   ]
 
   constructor(private sanitization: DomSanitizer) { }
