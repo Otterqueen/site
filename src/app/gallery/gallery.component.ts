395532cd8af978d4ec43e05f50b172d96afddd93
Unknown <mchapard@student.le-101.fr>
2019-05-29 19:39:49 +0200

    galery opti

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'gallery',
@@ -9,16 +10,27 @@ export class GalleryComponent implements OnInit {
 
   @Input() src_list : Array<any>;
 
-  constructor() { }
+  constructor(private sanitization: DomSanitizer) { }
 
   ngOnInit() {
     console.log("list = ", this.src_list);
   }
 
   show_viewer(img_src){
-    console.log("src = ", img_src.changingThisBreaksApplicationSecurity);
+    
     document.getElementById('canvas').style.display = 'inline-flex';
     document.getElementById('canvas').style.backgroundImage = img_src.changingThisBreaksApplicationSecurity;
+    
+    
+    
+    // var regex = /(\d)/;
+
+    // //console.log("src = ", img_src.changingThisBreaksApplicationSecurity.split(regex));
+    // //console.log("this san truc = ",this.sanitization.bypassSecurityTrustStyle(`url('assets/photos/${tab[1]+tab[2]}')`));
+    // var tab =img_src.changingThisBreaksApplicationSecurity.split(regex);
+    // //console.log(tab)
+    // document.getElementById('canvas').style.backgroundImage = 'assets/photos/'+tab[1]+tab[2];
+    // console.log("src backgrou = ",document.getElementById('canvas').style)
   }
 
   hide_viewer(){
